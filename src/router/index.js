527d9223c57bfb962539667122b5b7fa441d5df5
Mike Morton <mmorton7295@gmail.com>
2021-07-25 10:24:49 -0500

    adds puppies index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import PuppiesIndex from '../views/PuppiesIndex.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -11,19 +13,21 @@ const routes = [
     component: Home
   },
   {
+    path: '/puppies',
+    name: 'Puppies Index',
+    component: PuppiesIndex
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/PuppiesIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>Puppies</h1>
+    <hr />
+    <div v-for="puppy in puppies">
+      <p> Name: {{ puppy.name }} </p>
+      <p> Age (years): {{ puppy.age }} </p>
+      <p> Breed: {{ puppy.breed }} </p>
+      <hr />
+    </div>
+
+  </div>
+</template>
+
+<style>
+img {
+  height: 250px;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      puppies: [],
+    };
+  },
+  created: function () {
+    this.puppiesIndex();
+  },
+  methods: {
+    puppiesIndex: function () {
+      axios.get("http://localhost:3000/puppies").then((response) => {
+        this.puppies = response.data;
+      });
+    },
+  },
+};
+</script>
