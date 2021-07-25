3b884d9e3397d5bd1c57e96a7267aef5a9551625
Mike Morton <mmorton7295@gmail.com>
2021-07-25 12:30:52 -0500

    adds sorting functionality to index

--- src/views/PuppiesIndex.vue
+++ src/views/PuppiesIndex.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
     <h1>Puppies</h1>
-    <hr />
+    <p>Sort by: </p> <button v-on:click="sortById()">ID</button> <button v-on:click="sortByName()">Name</button>
+    <hr /> 
     <div v-for="puppy in puppies">
+      <p>id: {{ puppy.id }}</p>
       <p> Name: {{ puppy.name }} </p>
       <p> Age (years): {{ puppy.age }} </p>
       <p> Breed: {{ puppy.breed }} </p>
@@ -30,11 +32,20 @@ export default {
   created: function () {
     this.puppiesIndex();
   },
+  // computed: function (sortMethod) {
+  //   sortedPuppies: sortMethod(this.puppies);
+  // },
   methods: {
     puppiesIndex: function () {
       axios.get("/puppies").then((response) => {
         this.puppies = response.data;
       });
+    },
+    sortById: function () {
+      this.puppies.sort((a, b) => a.id - b.id);
+    },
+    sortByName: function () {
+      this.puppies.sort((a, b) => a.name.localeCompare(b.name));
     },
   },
 };
